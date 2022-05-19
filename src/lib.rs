5a790a7a64d39ba06dadd5fb923ae482fb741938
Sébastien LUNDI <slundi@gmail.com>
2022-05-19 11:03:08 +0200

    lot of fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-use ureq::{Agent, AgentBuilder};
+use ureq::Agent;
 use std::time::Duration;
 
 const BASE_URL: &str = "https://www.vivino.com/api/";
 const RECORDS_PER_PAGE: u16 = 25;
 const USER_AGENT: &str = "";
 
-pub struct Payload {
-    /// "country_codes[]": "br",
-    pub country_codes: Option<Vec<String>>,
+pub struct Payload<'a> {
+    /// "country_codes[]": "br","fr","us","de"
+    pub country_codes: Option<Vec<&'a str>>,
     /// "food_ids[]": 20,
-    pub food_ids: Option<u16>,
+    pub food_ids: Option<Vec<u16>>,
     /// "grape_ids[]": 3,
-    pub grape_ids: Option<u16>,
+    pub grape_ids: Option<Vec<u16>>,
     /// "grape_filter": "varietal",
     pub grape_filter: Option<String>,
     /// "min_rating": 3.7, from 0.0 to 5.0
@@ -37,12 +37,12 @@ pub struct Payload {
     pub wine_type_ids: Option<u16>,
 }
 
-impl Default for Payload {fn default() -> Self {Payload {
-    country_codes: Some(vec![String::from("en"), String::from("fr")]),
+impl Default for Payload<'_> {fn default() -> Self {Payload {
+    country_codes: Some(vec!["us", "fr"]),
     food_ids: None,
     grape_ids: None,
     grape_filter: None,
-    min_rating: 3.7,
+    min_rating: Some(3.7),
     order_by: None,
     order: None,
     price_range_min: None,
@@ -52,73 +52,83 @@ impl Default for Payload {fn default() -> Self {Payload {
     wine_type_ids: None,
 }}}
 
-pub fn get(payload: &Payload) {
+pub fn get(payload: &Payload) -> Result<ureq::Response, ureq::Error> {
     let mut url = String::from(BASE_URL);
     let agent: Agent = ureq::AgentBuilder::new()
         .timeout_read(Duration::from_secs(120))
         .timeout_write(Duration::from_secs(10))
-        .agent(r#""#)
+        .user_agent(USER_AGENT)
         .build();
     url.push_str("explore/explore?");
-    if Some(x) = payload.country_codes {
-        url.push_str("&country_codes[]=");
-        for y in x {url.push_str(&y); url.push(',');}
-        url.pop();
+    if let Some(x) = &payload.country_codes {
+        for y in x {
+            url.push_str("&country_codes[]=");
+            url.push_str(y);
+        }
     }
-    if Some(x) = payload.food_ids {
-        url.push_str("&food_idss[]=");
-        for y in x {url.push_str(&y.to_string()); url.push(',');}
-        url.pop();
+    if let Some(x) = &payload.food_ids {
+        for y in x {
+            url.push_str("&food_ids[]=");
+            url.push_str(&y.to_string());
+        }
     }
-    if Some(x) = payload.grape_ids {
-        url.push_str("&grape_ids[]=");
-        for y in x {url.push_str(&y.to_string()); url.push(',');}
-        url.pop();
+    if let Some(x) = &payload.grape_ids {
+        for y in x {
+            url.push_str("&grape_ids[]=");
+            url.push_str(&y.to_string());
+        }
     }
-    if Some(x) = payload.grape_filter {
+    if let Some(x) = &payload.grape_filter {
         url.push_str("&grape_filter=");
-        url.push_str(&x);
+        url.push_str(x);
     }
-    if Some(x) = payload.min_rating {
-        url.push_str("&min_rating");
-        url.push_str(&y.to_string());
+    if let Some(x) = &payload.min_rating {
+        url.push_str("&min_rating=");
+        url.push_str(&x.to_string());
     }
-    if Some(x) = payload.order_by {
+    if let Some(x) = &payload.order_by {
         url.push_str("&order_by=");
-        {url.push_str(&y);
+        url.push_str(x);
     }
-    if Some(x) = payload.order {
+    if let Some(x) = payload.order {
         url.push_str("&order=");
         url.push_str(if x {"asc"} else {"desc"});
     }
-    if Some(x) = payload.price_range_min {
-        url.push_str("&price_range_min");
-        url.push_str(&y.to_string());
+    if let Some(x) = payload.price_range_min {
+        url.push_str("&price_range_min=");
+        url.push_str(&x.to_string());
     }
-    if Some(x) = payload.price_range_max {
-        url.push_str("&price_range_max");
-        url.push_str(&y.to_string());
+    if let Some(x) = payload.price_range_max {
+        url.push_str("&price_range_max=");
+        url.push_str(&x.to_string());
     }
-    if Some(x) = payload.region_ids {
-        url.push_str("&region_ids[]=");
-        for y in x {url.push_str(&y.to_string()); url.push(',');}
-        url.pop();
+    if let Some(x) = &payload.region_ids {
+        for y in x {
+            url.push_str("&region_ids[]=");
+            url.push_str(&y.to_string());
+        }
     }
-    if Some(x) = payload.wine_style_ids {
-        url.push_str("&wine_style_ids[]=");
-        for y in x {url.push_str(&y.to_string()); url.push(',');}
-        url.pop();
+    if let Some(x) = &payload.wine_style_ids {
+        for y in x {
+            url.push_str("&wine_style_ids[]=");
+            url.push_str(&y.to_string());
+        }
+        
     }
-    url.replace("?&","?");
-    agent.get(&url).call();
+    agent.get(&url.replace("?&","?")).call()
 }
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let pl = Payload{country_codes:Some(vec!["fr","us"]), ..Default::default()};
+        let out = get(&pl);
+        let res = out.unwrap();
+        assert!(res.status() == 200, "{:?}", res);
+        println!("{:?}", res.into_string());
     }
 }
