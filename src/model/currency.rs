5ffc3ad1312cbe66c9e57c36442b4dbd23913f3e
Sébastien LUNDI <slundi@gmail.com>
2023-02-12 10:18:04 +0100

    Currency parsing test

--- src/model/currency.rs
+++ src/model/currency.rs
@@ -7,3 +7,19 @@ pub struct Currency {
     prefix: Option<String>,
     suffix: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use serde_json;
+
+    #[test]
+    fn test_deserialize_activity() {
+        let json =
+            "{ \"code\": \"EUR\", \"name\": \"Euros\", \"prefix\": \"€\", \"suffix\": null }";
+        let c: crate::model::currency::Currency = serde_json::from_str(json).unwrap();
+        assert!(c.code == "EUR");
+        assert!(c.name == "Euros");
+        assert!(c.prefix == Some(String::from("€")));
+        assert!(c.suffix.is_none());
+    }
+}
